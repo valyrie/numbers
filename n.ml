a71428d30ddd9a4220c2b5a6ca8a6fe3bbced268
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-11-05 22:59:55 +0100

    renamed variables in n.ml to be more consistent

--- n.ml
+++ n.ml
@@ -1,24 +1,24 @@
 (* natural numbers: 0, 1, 2, 3, n *)
 type t = int list
-let rec is_zero (x: t) =
-    match x with
+let rec is_zero (n: t) =
+    match n with
     [] -> true
     | 0 :: tl -> is_zero tl
     | _ -> false
-let rec succ (x: t): t =
-    match x with
+let rec succ (n: t): t =
+    match n with
         [] | [0] -> [1]
         | hd :: tl ->
             if hd < Int.max_int then
                 hd + 1 :: tl
             else
                 0 :: succ tl
-let rec pred (x: t): t =
-    match x with
+let rec pred (n: t): t =
+    match n with
     [] | [0] -> raise (Invalid_argument "0 has no predecessor")
     | 1 :: [] -> []
-    | 0 :: m -> Int.max_int :: pred m
-    | n :: m -> n - 1 :: m
+    | 0 :: tl -> Int.max_int :: pred tl
+    | hd :: tl -> hd - 1 :: tl
 let of_int i: t =
     [i]
 let to_int n =
