12d37e815ef25f339850148ac59a0add8ab148c9
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-11-06 04:04:51 +0100

    wrote most of a new implementation of natural numbers, designed to be very faaaaast. has add/sub/mul, and many supporting functions.

--- n.ml
+++ n.ml
@@ -1,50 +1,98 @@
 (* natural numbers: 0, 1, 2, 3, n *)
-type t = int list
-let rec is_zero (n: t) =
-    match n with
-    [] -> true
-    | 0 :: tl -> is_zero tl
-    | _ -> false
-let rec succ (n: t): t =
-    match n with
-        [] | [0] -> [1]
-        | hd :: tl ->
-            if hd < Int.max_int then
-                hd + 1 :: tl
-            else
-                0 :: succ tl
-let rec pred (n: t): t =
-    match n with
-    [] | [0] -> raise (Invalid_argument "0 has no predecessor")
-    | 1 :: [] -> []
-    | 0 :: tl -> Int.max_int :: pred tl
-    | hd :: tl -> hd - 1 :: tl
-let of_int i: t =
-    [i]
-let to_int n =
-    match n with
-        [] -> 0
-        | [i] -> i
-        | _ -> raise (Invalid_argument "natural is too large to be converted to an int")
-let rec compare x y =
-    match x, y with
-        [0], _ -> -1
-        | _, [0] -> 1
-        | [n], [m] -> Int.compare n m 
-        | _, _ -> match (is_zero x, is_zero y) with
-            true, true -> 0
-            | true, false -> -1
-            | false, true -> 1
-            | false, false -> compare (pred x) (pred y)
-let rec add x y =
-    match x, y with
-        _, [0] -> x
-        | n :: ntl, m :: mtl when n != 0 && m != 0 -> let d = min (Int.max_int - n) m in 
-            add (n + d :: ntl) (m - d :: mtl)
-        | _, _ -> add (succ x) (pred y)
-let rec sub x y =
-    match x, y with
-        _, [0] -> x
-        | n :: ntl, m :: mtl when n != 0 && m != 0 -> let d = min n m in
-            sub (n - d :: ntl) (m - d :: mtl)
-        | _, _ -> sub (pred x) (pred y)
+
+type t = bytes
+let make i n =
+    Bytes.make i @@ Char.chr n
+let u8 i = Int.logand i 0xFF
+let u8carry i = Int.shift_right_logical i 8
+let to_list n =
+    List.map Char.code @@ List.of_seq @@ Bytes.to_seq n
+let fold_left f i n =
+    List.fold_left f i @@ to_list n
+let fold_right f i n =
+    List.fold_right f (to_list n) i
+let make_zero i = make i 0
+let zero = make_zero 1
+let is_zero n =
+    fold_left (fun zero byte -> zero && byte = 0) true n
+let length n =
+    Bytes.length n
+let ctz_digits n =
+    fold_left (fun s i -> if i != 0 then 0 else s + 1) 0 n
+let significant_digits n =
+    max 1 @@ length n - ctz_digits n
+let trim n =
+    Bytes.sub n 0 @@ significant_digits n
+let to_string n =
+    let to_string_inner n =
+        fold_left (fun s i -> Printf.sprintf "%02x%s" i s) "" @@ trim n in
+    Printf.sprintf "0x%s" @@ to_string_inner n
+let get n i =
+    if i < length n then
+        Bytes.get n i
+    else
+        Char.chr 0
+let pad_to l a b =
+    let l = max l @@ max (length a) (length b) in
+    Bytes.init l (get a), Bytes.init l (get b)
+let fold_left2 f i l a b =
+    let (a, b) = pad_to l a b in
+    List.fold_left2 f i
+        (to_list a)
+        (to_list b)
+let fold_right2 f a b i l=
+    let (a, b) = pad_to l a b in
+    List.fold_right2 f
+        (to_list a)
+        (to_list b) i
+let add a b =
+    let l = 1 + max (length a) (length b) in
+    let (c, _, _) = fold_left2
+        (fun (c, i, carry) a b ->
+            let cn = a + b + carry in
+            Bytes.set_uint8 c i @@ u8 cn;
+            c, i + 1, u8carry cn)
+        (make l 0, 0, 0) l a b in
+    c
+let compare a b =
+    fold_left2
+        (fun c a b -> if Int.compare a b = 0 then c else Int.compare a b) 0 0 a b
+let sub a b =
+    if compare a b > 0 then
+        let l = max (length a) (length b) in
+        let (c, _, _) = fold_left2
+            (fun (c, i, carry) a b ->
+                let cn = a - b - carry in
+                Bytes.set_uint8 c i @@ u8 cn;
+                c, i + 1, u8carry cn)
+            (make l 0, 0, 0) l a b in
+        c
+    else if compare a b = 0 then
+        zero
+    else
+        raise @@ Invalid_argument (Printf.sprintf "Cannot subtract N %s N %s" (to_string b) (to_string a))
+let mul a b =
+    let l = (length a) + (length b) in
+    let (c, _, _) = fold_left2
+        (fun (c, i, carry) a b ->
+            let cn = a * b + carry in
+            Bytes.set_uint8 c i @@ u8 cn;
+            c, i + 1, u8carry cn)
+        (make l 0, 0, 0) l a b in
+    c
+let is_couting n =
+    compare n zero > 0
+let cat a b =
+    Bytes.cat a b
+let addu32 n i =
+    let b = make_zero 4 in
+    let _ = Bytes.set_int32_ne b 0 i in
+    add n b
+let addu64 n i =
+    let b = make_zero 8 in
+    let _ = Bytes.set_int64_ne b 0 i in
+    add n b
+let of_u32 i =
+    addu32 zero i
+let of_u64 i =
+    addu64 zero i
