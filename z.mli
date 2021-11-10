a3d1909590dff7f5f1e72e6bd4a3f285904afcf2
Valyrie Jacqueline Heather Autumn <avalyrie@gmail.com>
2021-11-10 18:51:41 +0000

    generated mli for z

---
+++ z.mli
@@ -0,0 +1,33 @@
+type t
+exception Underflow
+exception Division_by_zero
+val zero : t
+val one : t
+val minus_one : t
+val trim : t -> t
+val to_string : t -> string
+val compare : t -> t -> int
+val is_zero : t -> bool
+val is_negative : t -> bool
+val is_positive : t -> bool
+val neg : t -> t
+val add : t -> t -> t
+val sub : t -> t -> t
+val succ : t -> t
+val pred : t -> t
+val double : t -> t
+val half : t -> t
+val mul : t -> t -> t
+val divrem : t -> t -> t * t
+val div : t -> t -> t
+val rem : t -> t -> t
+val of_i32 : int32 -> t
+val of_i64 : int64 -> t
+val of_int : int -> t
+val of_n : N.t -> t
+val of_k : K.t -> t
+val to_i32 : t -> int32
+val to_i64 : t -> int64
+val to_int : t -> int
+val to_n : t -> N.t
+val to_k : t -> K.t
